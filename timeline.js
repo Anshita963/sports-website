e84eeb3468563d41b6f5b274ca86c3886aaf153d
Anshita963 <88958121+Anshita963@users.noreply.github.com>
2021-09-20 18:53:44 +0530

    Add files via upload

---
+++ timeline.js
@@ -0,0 +1,15 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyp3xnk9xeiF79uFue3VXvhd5b8RMTGzs",
+  authDomain: "sportswebsite-479f5.firebaseapp.com",
+  databaseURL: "https://sportswebsite-479f5-default-rtdb.firebaseio.com",
+  projectId: "sportswebsite-479f5",
+  storageBucket: "sportswebsite-479f5.appspot.com",
+  messagingSenderId: "1028764080078",
+  appId: "1:1028764080078:web:c475ceb12816df205f3d9f",
+  measurementId: "G-MKHHX3RCYP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+var userinputreference=
